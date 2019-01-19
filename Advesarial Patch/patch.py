1467c3e19545d4bc0a27b681fa2ac5d5b9d56f42
Jet New <notesjet@gmail.com>
2019-01-19 23:07:20 +0800

    add requirements.txt

--- Advesarial Patch/patch.py
+++ Advesarial Patch/patch.py
@@ -3,7 +3,7 @@ import cv2
 
 def patch_attack(original_img,patch,resize=False):
 	"""
-	Patch performs an advserial patch black box attack on a neural network
+	Perform an adversarial patch black box attack on a neural network
 	"""
 	print("SIZES", original_img.shape) # (2308,3468,3) GIBBON
 	# (1026,778,3) TOTORO
@@ -15,14 +15,14 @@ def patch_attack(original_img,patch,resize=False):
 	patch = cv2.resize(patch, dsize=(size, size), interpolation=cv2.INTER_CUBIC)
 	print("SIZES", patch.shape) # (662,838,4)
 
-	x_offset = 50
-	y_offset = 50
+	x_offset = 500
+	y_offset = 500
 
 	y1, y2 = y_offset, y_offset + patch.shape[0]
-	print("y1, y2", y1, y2) #(50,712)
+	print("y1, y2", y1, y2)  # (50,712)
 
 	x1, x2 = x_offset, x_offset + patch.shape[1]
-	print("x1, x2", x1, x2) #(50,888)
+	print("x1, x2", x1, x2)  # (50,888)
 
 	assert patch.shape[2] == 4, "Make sure you use cv2.imread('patch.png',-1)"
 	alpha_s = patch[:, :, 3] / 255.0
