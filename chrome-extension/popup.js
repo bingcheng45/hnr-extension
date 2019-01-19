9af5e760d49ac773437e93b921b4e76e362cba51
Chua Bing Quan <chipneverdies@gmail.com>
2019-01-20 00:40:03 +0800

    Logic for droplist completed.

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -1,10 +1,23 @@
 // Wait for DOM to be ready
 document.addEventListener('DOMContentLoaded', () => {
+  const API_URL = 'http://localhost:5000/api/attack';
+
   const dropdownItems = document.getElementsByClassName('dropdown-item');
 
   for (let item of dropdownItems) {
     item.addEventListener('click', function (e) {
       document.getElementById('type').lastElementChild.innerText = this.getAttribute('data-label');
+
+      const views = document.getElementsByClassName('view');
+      const viewToRender = this.getAttribute('data-view');
+      
+      for (let view of views) {
+        if (view.getAttribute('id') === viewToRender) {
+          view.classList.remove('hide');
+        } else {
+          view.classList.add('hide');
+        }
+      }
     });
   }
 
@@ -16,9 +29,32 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   // Submits textarea input to backend
-  document.getElementById('generate-text-btn').addEventListener('click', () => {
-    const textInput = document.getElementById('text');
+  document.getElementById('generate-text-btn').addEventListener('click', async function () {
+    let textInput;
 
+    try {
+      console.log('start');
+      textInput = document.getElementById('text');
+
+      this.setAttribute('disabled', true);
+      textInput.setAttribute('disabled', true);
+      this.innerHTML = 'Generating...';
+  
+      /*
+      // make async call to call server
+      const resp = await fetch(API_URL, {
+        method: 'POST',
+        cache: 'no-cache'
+      });
+      */
+
+    } catch (err) {
+      console.log(err);
+    } finally {
+      this.removeAttribute('disabled');
+      textInput.removeAttribute('disabled');
+      this.innerHTML = 'Generate';
+    }
   });
 }, false);
 
