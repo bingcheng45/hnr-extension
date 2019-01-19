040d644aead8c252dd1cf7699e95190d4f1aa49c
Ang Ming Liang <angmingliang4017ic@gmail.com>
2019-01-19 14:12:51 +0800

    updated

--- densenet_attack.py
+++ densenet_attack.py
@@ -1,11 +1,11 @@
-from keras.applications.densenet import DenseNet201, decode_predictions
+from keras.applications.densenet import DenseNet201, preprocess_input,decode_predictions
 from keras.preprocessing import image
 from keras.models import Model
 import numpy as np 
 
 base_model = DenseNet201(include_top=True, weights='imagenet', input_tensor=None, input_shape=None, pooling=None, classes=1000)
 
-img_path = "gibbon.png"
+img_path = "gibbon.jpg"
 img = image.load_img(img_path, target_size=(224, 224))
 x = image.img_to_array(img)
 x = np.expand_dims(x, axis=0)
