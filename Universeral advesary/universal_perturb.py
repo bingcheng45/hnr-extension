1b094e2100f38169613602efae12d24b28576ed7
Ang Ming Liang <angmingliang4017ic@gmail.com>
2019-01-20 03:53:41 +0800

    update univ peturb

--- Universeral advesary/universal_perturb.py
+++ Universeral advesary/universal_perturb.py
@@ -18,14 +18,14 @@ def rand_other(pred):
 		new_pred = random.randint(0,199)
 	return new_pred
 
-def fast_signed_gradient(model,x,y,e=0.20):
+def fast_signed_gradient(model,x,y,e=5):
 	theta = model.trainable_weights[0]
 	outputTensor = model.output
 	label= tf.constant([0.0 if i!= y else 1.0 for i in range(1000)],dtype=tf.float32)
 	loss = K.categorical_crossentropy(outputTensor,label, from_logits=False)
-	grads = K.gradients(loss, model.input)[0]
+	peturb = e*K.sign(K.gradients(loss, model.input)[0])
 	sess.run(tf.initialize_all_variables())
-	return sess.run(grads,feed_dict={model.input:x})
+	return sess.run(peturb,feed_dict={model.input:x})
 
 base_model = VGG16(include_top=True, weights='imagenet', input_tensor=None, input_shape=None, pooling=None, classes=1000)
 train_datagen = ImageDataGenerator(shear_range=0.2,
@@ -42,7 +42,9 @@ delta = 0.1
 gamma = 3000
 v = np.zeros((1,224,224,3))
 while err<=1-delta:
+	print(err)
 	for i,z in tqdm(enumerate(train_generator)):
+		print(i,err)
 		x_i,y_i = z
 		x_i = preprocess_input(x_i)
 		pred = base_model.predict(x_i)
@@ -51,8 +53,8 @@ while err<=1-delta:
 
 			r = fast_signed_gradient(base_model,x_i,rand_other(pred))
 			pred_v = base_model.predict(x_i + v+ r)
+			print(np.max(pred),np.max(pred_v))
 			if np.argmax(pred) == np.argmax(pred_v): 
-				err +=1
 				continue # Escape if the fast signed method fails
 
 			while np.argmax(pred) != np.argmax(pred_v):
@@ -60,10 +62,12 @@ while err<=1-delta:
 				pred_v = base_model.predict(x_i + v+ r)
 			r/=0.9
 			v_prime=v+r 
+			err +=1
 			# Dubious, really dubious implementation of the projection
 			# We can also use a pesudo inverse to calculate the projection
 			# Rmb linear regression
 			v = np.dot(v,v_prime)/(np.linalg.norm(v))
+			#v = np.linalg.inv(v@v.T) @ v.T@v_prime
 			if np.linalg.norm(v) > gamma: v *= gamma/np.linalg.norm(v)
 
 with open("univseral_pertub.pkl","rb") as f:
