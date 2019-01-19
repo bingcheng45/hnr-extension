eedcdaaf545e5745d494a96c8b8d069f7a6fe4ab
Ang Ming Liang <angmingliang4017ic@gmail.com>
2019-01-20 04:53:27 +0800

    update

--- Universeral advesary/densenet.py
+++ Universeral advesary/densenet.py
@@ -1,5 +1,5 @@
 import numpy as np 
-import cv2
+import matplotlib.pyplot as plt
 from keras.applications.densenet import DenseNet201, preprocess_input, decode_predictions
 from keras.preprocessing import image
 from keras.models import Model
@@ -8,26 +8,21 @@ from universal_perturb import rand_other,fast_signed_gradient
 base_model = DenseNet201(include_top=True, weights='imagenet', input_tensor=None, input_shape=None, pooling=None, classes=1000)
 
 img_path = "gibbon.jpg"
+img_orginal = image.load_img(img_path)
 img = image.load_img(img_path, target_size=(224, 224))
+x_org = image.img_to_array(img_orginal)
 x = image.img_to_array(img)
 x = np.expand_dims(x, axis=0)
 x = preprocess_input(x)
 
-original_img = cv2.imread("gibbon.jpg")
-patch = cv2.imread("patch.png", -1)
-
-# cv2.imshow('image', patch)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-
-
 pred = base_model.predict(x)
 
-x_pos = fast_signed_gradient(original_img, rand_other(pred))
-cv2.imshow("image_pos",x_pos)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
+x_pos = fast_signed_gradient(x, rand_other(pred))
+x_pos = x_org+np.resize(x_pos,(x_org.shape[0],x_org.shape[1],3))
+plt.imshow(x_org)
+plt.show()
+plt.imshow(x_pos)
+plt.show()
 # x_pos = np.expand_dims(x_pos, axis=0)
 # x_pos = preprocess_input(x_pos)
 
