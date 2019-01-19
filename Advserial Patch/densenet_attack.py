7c8399b3693edbdb56baac6e2b1ebadd43104c3b
Ang Ming Liang <angmingliang4017ic@gmail.com>
2019-01-19 15:43:00 +0800

    Added patch.py

--- Advserial Patch/densenet_attack.py
+++ Advserial Patch/densenet_attack.py
@@ -1,7 +1,9 @@
+import numpy as np 
+import cv2
 from keras.applications.densenet import DenseNet201, preprocess_input,decode_predictions
 from keras.preprocessing import image
 from keras.models import Model
-import numpy as np 
+from patch import patch_attack
 
 base_model = DenseNet201(include_top=True, weights='imagenet', input_tensor=None, input_shape=None, pooling=None, classes=1000)
 
@@ -11,5 +13,14 @@ x = image.img_to_array(img)
 x = np.expand_dims(x, axis=0)
 x = preprocess_input(x)
 
+original_img = cv2.imread("gibbon.jpg")
+patch = cv2.imread("patch.png",-1)
+x_pos = patch_attack(original_img,patch)
+x_pos = np.expand_dims(x_pos, axis=0)
+x_pos = preprocess_input(x_pos)
+
 pred = base_model.predict(x)
-print(decode_predictions(preds, top=3)[0])
+pred_pos = base_model.predict(x_pos)
+
+print("Un-poisoned image: {}".format(decode_predictions(pred, top=3)[0]))
+print("Advserial image: {}".format(decode_predictions(pred_pos,top=3)[0]))
