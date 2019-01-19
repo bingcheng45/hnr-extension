8d5e5cdf4c5379d6989b2ff9c511ba4a9401298b
Ang Ming Liang <angmingliang4017ic@gmail.com>
2019-01-20 03:55:48 +0800

    update

--- Universeral advesary/universal_perturb.py
+++ Universeral advesary/universal_perturb.py
@@ -10,8 +10,6 @@ from keras.preprocessing import image
 from keras.models import Model
 from keras import backend as K
 
-sess = tf.Session()
-
 def rand_other(pred):
 	new_pred = np.argmax(pred)
 	while np.argmax(pred) == new_pred:
@@ -37,38 +35,41 @@ train_generator = train_datagen.flow_from_directory(
         target_size=(224, 224),
         batch_size=1)
 
-err = 0
-delta = 0.1
-gamma = 3000
-v = np.zeros((1,224,224,3))
-while err<=1-delta:
-	print(err)
-	for i,z in tqdm(enumerate(train_generator)):
-		print(i,err)
-		x_i,y_i = z
-		x_i = preprocess_input(x_i)
-		pred = base_model.predict(x_i)
-		pred_v = base_model.predict(x_i + v)
-		if np.argmax(pred) == np.argmax(pred_v):
+if __name__ == '__main__':
+	sess = tf.Session()
 
-			r = fast_signed_gradient(base_model,x_i,rand_other(pred))
-			pred_v = base_model.predict(x_i + v+ r)
-			print(np.max(pred),np.max(pred_v))
-			if np.argmax(pred) == np.argmax(pred_v): 
-				continue # Escape if the fast signed method fails
+	err = 0
+	delta = 0.1
+	gamma = 3000
+	v = np.zeros((1,224,224,3))
+	while err<=1-delta:
+		print(err)
+		for i,z in tqdm(enumerate(train_generator)):
+			print(i,err)
+			x_i,y_i = z
+			x_i = preprocess_input(x_i)
+			pred = base_model.predict(x_i)
+			pred_v = base_model.predict(x_i + v)
+			if np.argmax(pred) == np.argmax(pred_v):
 
-			while np.argmax(pred) != np.argmax(pred_v):
-				r= r*0.9
+				r = fast_signed_gradient(base_model,x_i,rand_other(pred))
 				pred_v = base_model.predict(x_i + v+ r)
-			r/=0.9
-			v_prime=v+r 
-			err +=1
-			# Dubious, really dubious implementation of the projection
-			# We can also use a pesudo inverse to calculate the projection
-			# Rmb linear regression
-			v = np.dot(v,v_prime)/(np.linalg.norm(v))
-			#v = np.linalg.inv(v@v.T) @ v.T@v_prime
-			if np.linalg.norm(v) > gamma: v *= gamma/np.linalg.norm(v)
+				print(np.max(pred),np.max(pred_v))
+				if np.argmax(pred) == np.argmax(pred_v): 
+					continue # Escape if the fast signed method fails
 
-with open("univseral_pertub.pkl","rb") as f:
-	pkl.dump(v,f)
+				while np.argmax(pred) != np.argmax(pred_v):
+					r= r*0.9
+					pred_v = base_model.predict(x_i + v+ r)
+				r/=0.9
+				v_prime=v+r 
+				err +=1
+				# Dubious, really dubious implementation of the projection
+				# We can also use a pesudo inverse to calculate the projection
+				# Rmb linear regression
+				v = np.dot(v,v_prime)/(np.linalg.norm(v))
+				#v = np.linalg.inv(v@v.T) @ v.T@v_prime
+				if np.linalg.norm(v) > gamma: v *= gamma/np.linalg.norm(v)
+
+	with open("univseral_pertub.pkl","rb") as f:
+		pkl.dump(v,f)
